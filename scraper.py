e96fdb0ea1cc31d7a093f597d768a5e81364c143
larawhyte <larawhyte@gmail.com>
2016-01-21 11:38:19 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ html = scraperwiki.scrape("http://uk.soccerway.com/teams/netherlands/fortuna-sit
 
 root = lxml.html.fromstring(html)
 #drill into the specific table you are looking for - the <td> data - this can change depending on what you want to look for
-root.cssselect('tds')
+tds=root.cssselect('td')
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
